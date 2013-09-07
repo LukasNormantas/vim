3b14c60ecc9a8f3b3182ac345723be20eb6e2adc
Lukas Normantas <baciuvys@gmail.com>
2013-09-07 01:40:24 +0100

    Script improved.

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+# This script links system VIM config files to the content of 
+# this directory, it only has to be ran once after cloning
+# this directory from git.
+
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 INITIAL_DIR=`pwd`
+
+cd $SCRIPT_DIR
+git submodule update
+
 cd ~
 rm -rf ~/.vim && ln -s $SCRIPT_DIR/.vim ~/.vim
 rm ~/.vimrc && ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
-cd $SCRIPT_DIR
-git submodule update
+
+cd $INITIAL_DIR
